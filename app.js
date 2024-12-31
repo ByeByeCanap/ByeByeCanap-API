fa8a11c4fd2785b510be8d29670f4d39b6cafb25
ThisIsLuckyluck <denislucas1a@gmail.com>
2024-12-31 15:05:38 +0100

    updating swagger config on app.js

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const { swaggerSpec, swaggerUi } = require("./configs/SwaggerConfig");
+const CSS_URL =
+  "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
 require("dotenv").config();
 
 // Routes import
@@ -21,7 +23,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use("/doc/api", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use(
+  "/doc/api",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerSpec, { customCssUrl: CSS_URL })
+);
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
